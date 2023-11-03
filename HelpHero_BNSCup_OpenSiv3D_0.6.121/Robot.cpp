51e99ed60e53057afc9dab65a9250b9bfa4af3f9
tagtagy <117792780+tagtagy@users.noreply.github.com>
2023-11-03 17:29:49 +0900

    a

---
+++ HelpHero_BNSCup_OpenSiv3D_0.6.121/Robot.cpp
@@ -0,0 +1,41 @@
+﻿#include "stdafx.h"
+#include "Robot.h"
+
+Robot::Robot(P2World* _world)
+{
+	area = robotRect;
+	world = _world;
+	//body = world->createRect(P2Dynamic, Vec2{ pos.x + robotRect.w / 2,pos.y + robotRect.h / 2 }, SizeF{ robotRect.w, robotRect.h }, {}, {});
+	timer.reset();
+}
+
+void Robot::Update()
+{
+	if (KeyShift.pressed()) {//Shift中はロボットの操作　プレイヤーは動かない
+		if (KeyA.pressed()) {
+			pos.x -= speed * Scene::DeltaTime();
+		}
+		if (KeyD.pressed()) {
+			pos.x += speed * Scene::DeltaTime();
+		}
+		if (KeyEnter.down()) {
+			pos.y += 400;
+		}
+		if (KeyEnter.up()) {
+			pos.y -= 400;
+		}
+
+	}
+	robotRect.x = pos.x;//反映
+	robotRect.y = pos.y;
+	area = robotRect;
+	area.h += 500;
+}
+
+void Robot::Draw()
+{
+	if (KeyShift.pressed()) {//掘削範囲
+		area.draw(ColorF{ 1,0.2,0,0.5 });
+	}
+	robotRect.draw(ColorF{ 1.0,1.0,0 });//腕
+}
---
+++ HelpHero_BNSCup_OpenSiv3D_0.6.121/Victim.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "common.h"
+class Victim//ちなみにvictimが被災者の英語らしい
+{
+	P2World* world;
+	Vec2 pos = { 700,450 };
+	bool carry=false;//運ばれてる
+	Rect victimRect = { 0,0,40,40 };
+public:
+	Victim();
+	void Update();
+	void Draw();
+
+	void carry_move(Vec2 _player); //掴まれている時ロボットに追従する
+	void carry_flip() { carry = !carry; }//掴まれているかの反転
+
+	Vec2 getPos() { return pos; }
+	Rect getRect() { return victimRect; }
+};
+
