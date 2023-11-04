d6ec058f0485259f472fbd0a9a9cda2491c27e37
timsakai <104742469+timsakai@users.noreply.github.com>
2023-11-04 18:23:31 +0900

    ステージバリエーションのクラス、中身カラで作成

---
+++ HelpHero_BNSCup_OpenSiv3D_0.6.121/StageDifinition.cpp
@@ -0,0 +1,32 @@
+﻿#include "stdafx.h"
+#include "StageDifinition.h"
+
+StageDifinition::StageDifinition()
+{
+}
+
+void StageDifinition::ResetWorld(P2World& _world)
+{
+}
+
+void StageDifinition::SetUpToWorld(P2World& _world, Player* _player, Victim* _victim, Array<Foothold*>* _footholds)
+{
+}
+
+int32 StageDifinition::GetStageNum()
+{
+	return -1;
+}
+
+Stage1::Stage1()
+{
+}
+
+void Stage1::SetUpToWorld(P2World& _world, Player* _player, Victim* _victim, Array<Foothold*>* _footholds)
+{
+}
+
+int32 Stage1::GetStageNum()
+{
+	return 1;
+}
---
+++ HelpHero_BNSCup_OpenSiv3D_0.6.121/StageDifinition.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "Foothold.h"
+#include "Player.h"
+#include "Robot.h"
+#include "Victim.h"
+
+//ステージのバリエーションづくり
+//ステージごとにStageDifinition派生クラスのインスタンスを一つづつ作成、グローバル変数なり共有変数にリストをつくる
+//狙ったStageDifinition派生クラスのインスタンスのSetUpToWorldを呼び出してステージをセットアップ
+
+//overrideでSetUpToWorldを上書きして、ステージごとに別の生成アルゴリズムを持つようにする。
+
+class StageDifinition
+{
+public:
+	StageDifinition();
+	void ResetWorld(P2World& _world);
+	virtual void SetUpToWorld(P2World& _world,Player* _player, Victim* _victim, Array<Foothold*>* _footholds);
+	virtual int32 GetStageNum();
+private:
+};
+
+class Stage1 : public StageDifinition
+{
+public:
+	Stage1();
+	void SetUpToWorld(P2World& _world, Player* _player, Victim* _victim, Array<Foothold*>* _footholds) override;
+	int32 GetStageNum() override;
+};
