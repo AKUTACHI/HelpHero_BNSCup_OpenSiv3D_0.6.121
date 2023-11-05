c4c2c2d4697ff57eb859ee16402a4d991bb20842
timsakai <104742469+timsakai@users.noreply.github.com>
2023-11-05 12:04:48 +0900

    タイトル画面のBGMを追加した

--- HelpHero_BNSCup_OpenSiv3D_0.6.121/Title.cpp
+++ HelpHero_BNSCup_OpenSiv3D_0.6.121/Title.cpp
@@ -1,9 +1,12 @@
 ﻿#include "stdafx.h"
 #include "Title.h"
+#include "Beeps.h"
 
 Title::Title(const InitData& init) : IScene{ init }
 {
 	Background = Texture{ U"textures/Title.png" };
+	Beeps::GetBeep(U"Title").setVolume(0.3);
+	Beeps::GetBeep(U"Title").play();
 }
 
 Title::~Title()
@@ -16,6 +19,8 @@ void Title::update()
 	{
 		// ストーリーシーンに遷移
 		changeScene(State::Game);
+		Beeps::GetBeep(U"Title").stop();
+		Beeps::GetBeep(U"TitleEnter").playOneShot();
 	}
 }
 
